f13b20e6480c99047fda1737ee11315540a052d6
JC <ms0242808@gmail.com>
2022-04-06 14:47:04 +0100

    landing v1.1

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,6 +1,58 @@
 <template>
-  <div class="about">
-    <h1>This is Contact us page</h1>
+  <div id="contact">
+    <b-row>
+      <b-col cols="3">
+        <h3>Contact us</h3>
+      </b-col>
+      <b-col cols="9">
+        email, socia medial
+      </b-col>
+    </b-row>
+    <b-row class="container">
+      <b-col>
+        <b-row>
+          <b-col cols="6" class="mt-2">
+            <b-form-input
+              id="input-2"
+              v-model="first"
+              placeholder="First name"
+              required
+            ></b-form-input>
+          </b-col>
+          <b-col cols="6" class="mt-2">
+            <b-form-input
+              id="input-2"
+              v-model="last"
+              placeholder="Surname"
+              required
+            ></b-form-input>
+          </b-col>
+          <b-col cols="12" class="mt-2">
+            <b-form-input
+              id="input-1"
+              v-model="email"
+              type="email"
+              placeholder="Email"
+              required
+            ></b-form-input>
+          </b-col>
+          <b-col cols="12" class="mt-2">
+            <b-form-textarea
+              id="textarea"
+              v-model="text"
+              placeholder="Messages..."
+              rows="3"
+              max-rows="6"
+            ></b-form-textarea>
+          </b-col>
+        </b-row>
+        <b-button class="mt-2" variant="outline-success">Send</b-button>
+      </b-col>
+      <b-col>
+        <h1>Map</h1>
+      </b-col>
+    </b-row>
+    
   </div>
 </template>
 
@@ -10,3 +62,9 @@ export default {
   name: 'Contact'
 }
 </script>
+
+<style>
+#contact{
+  min-height: 700px;
+}
+</style>
--- src/views/Faq.vue
+++ src/views/Faq.vue
@@ -1,49 +1,67 @@
 <template>
-  <div class="faq">
-    <h1>Frequently Asked Questions</h1>
-    <b-container>
+  <div id="faq">
+    <h1 class="title">Frequently Asked Questions</h1>
+    <b-container class="mt-2">
       <b-row>
-        <b-col>
-          <h1>General</h1>
+        <b-col cols="5">
+          <h1 class="qtitle">General</h1>
         </b-col>
-        <b-col>
+        <b-col cols="7">
           <div class="accordion" role="tablist">
             <b-card no-body class="mb-1">
               <b-card-header header-tag="header" class="p-1" role="tab">
-                <b-button block v-b-toggle.accordion-1 variant="info">Accordion 1</b-button>
+                <b-button block v-b-toggle.accordion-1>Question 1</b-button>
               </b-card-header>
               <b-collapse id="accordion-1" visible accordion="my-accordion" role="tabpanel">
                 <b-card-body>
-                  
-                  <b-card-text>{{ text }}</b-card-text>
+                  <b-card-text>content</b-card-text>
                 </b-card-body>
               </b-collapse>
             </b-card>
-
             <b-card no-body class="mb-1">
               <b-card-header header-tag="header" class="p-1" role="tab">
-                <b-button block v-b-toggle.accordion-2 variant="info">Accordion 2</b-button>
+                <b-button block v-b-toggle.accordion-2>Question 2</b-button>
               </b-card-header>
               <b-collapse id="accordion-2" accordion="my-accordion" role="tabpanel">
                 <b-card-body>
-                  <b-card-text>{{ text }}</b-card-text>
+                  <b-card-text>content</b-card-text>
                 </b-card-body>
               </b-collapse>
             </b-card>
-
+          </div>
+        </b-col>
+        <b-col cols="5">
+          <h1 class="qtitle">Account</h1>
+        </b-col>
+        <b-col cols="7">
+          <div class="accordion" role="tablist">
             <b-card no-body class="mb-1">
               <b-card-header header-tag="header" class="p-1" role="tab">
-                <b-button block v-b-toggle.accordion-3 variant="info">Accordion 3</b-button>
+                <b-button block v-b-toggle.accordion-3>Question 1</b-button>
               </b-card-header>
-              <b-collapse id="accordion-3" accordion="my-accordion" role="tabpanel">
+              <b-collapse id="accordion-3" visible accordion="my-accordion" role="tabpanel">
                 <b-card-body>
-                  <b-card-text>{{ text }}</b-card-text>
+                  <b-card-text>content</b-card-text>
+                </b-card-body>
+              </b-collapse>
+            </b-card>
+            <b-card no-body class="mb-1">
+              <b-card-header header-tag="header" class="p-1" role="tab">
+                <b-button block v-b-toggle.accordion-4>Question 2</b-button>
+              </b-card-header>
+              <b-collapse id="accordion-4" accordion="my-accordion" role="tabpanel">
+                <b-card-body>
+                  <b-card-text>content</b-card-text>
                 </b-card-body>
               </b-collapse>
             </b-card>
           </div>
         </b-col>
       </b-row>
+      <div id="help">
+        <h3>Still need help?</h3>
+        <h5>Feel free to contact us</h5>
+      </div>
     </b-container>
   </div>
 </template>
@@ -56,5 +74,12 @@ export default {
 </script>
 
 <style>
+#help{
+  min-height: 550px;
+  margin-top: 5rem;
+}
 
+.qtitle{
+  border-top: solid 2px black;
+}
 </style>
